43a6de12e9f509185a0639d10f16cefc284906ce
pedrolopes94 <pms.lopes@campus.fct.unl.pt>
2016-05-04 13:30:53 +0100

    Added Erlang modules.

---
+++ src/recursive.erl
@@ -0,0 +1,18 @@
+-module(recursive).
+-export([zip/2,lenient_zip/2,tail_zip/3,tail_lenient_zip/3]).
+
+zip([],[]) -> [];
+zip([X|Xs],[Y|Ys]) -> [{X,Y}|zip(Xs,Ys)].
+
+lenient_zip([],_) -> [];
+lenient_zip(_,[]) -> [];
+lenient_zip([X|Xs],[Y|Ys]) -> [{X,Y}|lenient_zip(Xs,Ys)].
+
+tail_zip([],[],List) -> lists:reverse(List);
+tail_zip([X|Xs],[Y|Ys],List) ->
+	tail_zip(Xs,Ys,[{X,Y}|List]).
+
+tail_lenient_zip([],_,List) -> lists:reverse(List);
+tail_lenient_zip(_,[],List) -> lists:reverse(List);
+tail_lenient_zip([X|Xs],[Y|Ys],List) ->
+	tail_lenient_zip(Xs,Ys,[{X,Y}|List]).
